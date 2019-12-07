e4a90fbdb708414629b5fcc0c1c7a6982fc77ed2
Nathaniel Simmons <nlsimmons42@gmail.com>
2019-12-07 13:00:55 +0800

    Basic continent generation

--- World.py
+++ World.py
@@ -1,5 +1,9 @@
-# import random
-from pprint import pprint
+import random
+# from pprint import pprint
+
+
+def incrange(start, end):
+    return range(start, end + 1)
 
 
 class World(object):
@@ -10,7 +14,29 @@ class World(object):
 
         self.grid = [[0 for x in range(width)] for y in range(height)]
 
-        pprint(self.grid)
-
     def generate(self):
         pass
+
+    def generateContinent(self):
+        x = random.randint(0, self.width - 1)
+        y = random.randint(0, self.height - 1)
+
+        self.grid[y][x] = 1
+
+        queue = self.adjacent(x, y)
+        while len(queue):
+            random.shuffle(queue)
+            (x, y, v) = queue.pop()
+            if not self.grid[y][x] and random.randint(0, 100) < 30:
+                queue += self.adjacent(x, y)
+                self.grid[y][x] = 1
+
+    def adjacent(self, x, y):
+        square = []
+        for x in incrange(x - 1, x + 1):
+            for y in incrange(y - 1, y + 1):
+                try:
+                    square.append((x, y, self.grid[y][x]))
+                except IndexError:
+                    pass
+        return square
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from World import World
+from pprint import pprint
 
 world1 = World(10, 5)
 
-world1.generate()
+world1.generateContinent()
+
+pprint(world1.grid)
