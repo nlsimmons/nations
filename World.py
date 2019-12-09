63c9ebe919211a93ae612aebe2a277ca24cf71f3
Nathaniel Simmons <nlsimmons42@gmail.com>
2019-12-09 20:05:39 +0800

    Land gen starts at center

--- World.py
+++ World.py
@@ -5,21 +5,13 @@ from Tile import Tile
 
 
 class World(object):
-    """ Physical features of world map """
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.tiles = []
+        self.settlements = []
 
         self.grid = [[0 for x in range(width)] for y in range(height)]
-
-    def generateNations(self, num):
-        # pass
-        for _ in range(num):
-            self.generateNation()
-
-    def generateNation(self):
-        land = self.randomLand()
-        land.color = "red"
 
     def randomLand(self):
         while True:
@@ -29,24 +21,41 @@ class World(object):
             if self.grid[y][x]:
                 return self.grid[y][x]
 
+    def indexTiles(self):
+        self.tiles = []
+        for x in incrange(0, self.width - 1):
+            for y in incrange(0, self.height - 1):
+                if self.grid[y][x]:
+                    self.tiles.append(self.grid[y][x])
+
+        # for tile in self.tiles:
+        #     print( (tile.x, tile.y) )
+        # print(len(self.tiles))
+
     def generateContinent(self):
-        x = random.randint(0, self.width - 1)
-        y = random.randint(0, self.height - 1)
+        # x = random.randint(0, self.width - 1)
+        # y = random.randint(0, self.height - 1)
+        x = round((0 + self.width) / 2)
+        y = round((0 + self.height) / 2)
 
         self.grid[y][x] = Tile(x, y)
 
         queue = self.adjacent(x, y)
         while len(queue):
             random.shuffle(queue)
-            (x, y, v) = queue.pop()
-            if not self.grid[y][x] and random.randint(0, 100) < 30:
+            (x, y, tile) = queue.pop()
+            if self.grid[y][x] == 0 and random.randint(0, 100) < 30:
                 queue += self.adjacent(x, y)
                 self.grid[y][x] = Tile(x, y)
 
-    def adjacent(self, x, y):
+    def adjacent(self, x_start, y_start):
         square = []
-        for x in incrange(x - 1, x + 1):
-            for y in incrange(y - 1, y + 1):
+        for x in incrange(x_start - 1, x_start + 1):
+            for y in incrange(y_start - 1, y_start + 1):
+                if (x == x_start and y == y_start or
+                   x < 0 or x >= self.width or
+                   y < 0 or y >= self.height):
+                    continue
                 try:
                     square.append((x, y, self.grid[y][x]))
                 except IndexError:
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ random.seed(10)
 
 world1 = World(30, 15)
 world1.generateContinent()
+world1.indexTiles()
 
-world1.generateNations(10)
+# world1.generateNations(10)
 
 # pprint(world1.grid)
 
